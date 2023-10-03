7620898a58505beac6f148bd3af5011e102a0df2
ypVictorM <victor.masson@yeap.ai>
2023-10-03 13:52:29 +0200

    Add client + parser

---
+++ Tool/DataSource.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections.Generic;
+using CommandLine;
+using Npgsql;
+
+namespace PgSync;
+
+
+public class DataSource
+{
+    private readonly string _url = string.Empty;
+    private readonly string _name = string.Empty;
+    private readonly bool _debug;
+
+    private NpgsqlConnection _conn = new NpgsqlConnection();
+    private string _host = string.Empty;
+    private int? _port;
+    private string _dbname = string.Empty;
+    private List<Table> _tableSet = new List<Table>();
+
+    public DataSource(string url = "", string name = "", bool debug = false)
+    {
+        _url = url;
+        _name = name;
+        _debug = debug;
+    }
+
+    public bool Exists => !string.IsNullOrEmpty(_url);
+
+    public bool Local => string.IsNullOrEmpty(Host) || new[] { "localhost", "127.0.0.1" }.Contains(Host);
+
+    public string Host => _host ??= DedupLocalHost(Conninfo["host"]);
+
+    public int Port => _port ??= DedupLocalHost(Conninfo["port"]);
+
+    public string DbName => _dbname ??= Conninfo["dbname"];
+
+    public List<Table> Tables
+    {
+        get
+        {
+            if (_tableSet == null)
+            {
+                string query = @"
+                        SELECT
+                            table_schema AS schema,
+                            table_name AS table
+                        FROM
+                            information_schema.tables
+                        WHERE
+                            table_type = 'BASE TABLE' AND
+                            table_schema NOT IN ('information_schema', 'pg_catalog')
+                        ORDER BY 1, 2";
+                _tableSet = new List<Table>();
+
+                using (var connection = GetConnection())
+                using (var command = new NpgsqlCommand(query, connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string schema = reader.GetString(0);
+                        string table = reader.GetString(1);
+                        _tableSet.Add(new Table(schema, table));
+                    }
+                }
+            }
+            return _tableSet;
+        }
+    }
+
+    public bool TableExists(Table table)
+    {
+        return Tables.Contains(table);
+    }
+
+    public int MaxId(Table table, string primaryKey, string sqlClause = null)
+    {
+        string query = $"SELECT MAX({QuoteIdent(primaryKey)}) FROM {QuoteIdentFull(table)}{sqlClause}";
+        return ExecuteScalar<int>(query);
+    }
+
+    public int MinId(Table table, string primaryKey, string sqlClause = null)
+    {
+        string query = $"SELECT MIN({QuoteIdent(primaryKey)}) FROM {QuoteIdentFull(table)}{sqlClause}";
+        return ExecuteScalar<int>(query);
+    }
+
+    public string LastValue(string seq)
+    {
+        string query = $"SELECT last_value FROM {QuoteIdentFull(seq)}";
+        return ExecuteScalar<string>(query);
+    }
+
+    public void Truncate(Table table)
+    {
+        string query = $"TRUNCATE {QuoteIdentFull(table)} CASCADE";
+        Execute(query);
+    }
+
+    public List<string> Schemas
+    {
+        get
+        {
+            string query = "SELECT schema_name FROM information_schema.schemata ORDER BY 1";
+            return ExecuteList<string>(query, reader => reader.GetString(0));
+        }
+    }
+
+    public void CreateSchema(string schema)
+    {
+        string query = $"CREATE SCHEMA {QuoteIdent(schema)}";
+        Execute(query);
+    }
+
+    public List<Trigger> Triggers(Table table)
+    {
+        string query = @"
+                SELECT
+                    tgname AS name,
+                    tgisinternal AS internal,
+                    tgenabled != 'D' AS enabled,
+                    tgconstraint != 0 AS integrity
+                FROM
+                    pg_trigger
+                WHERE
+                    pg_trigger.tgrelid = $1::regclass";
+        return ExecuteList(query, new NpgsqlParameter("$1", table.FullName), reader => new Trigger
+        {
+            Name = reader.GetString(0),
+            Internal = reader.GetBoolean(1),
+            Enabled = reader.GetBoolean(2),
+            Integrity = reader.GetBoolean(3)
+        });
+    }
+
+    public NpgsqlConnection GetConnection()
+    {
+        if (_conn == null)
+        {
+            string connString = _url.StartsWith("postgres://") || _url.StartsWith("postgresql://")
+                ? _url
+                : $"dbname={_url}";
+            _conn = new NpgsqlConnection(connString);
+        }
+        return _conn;
+    }
+
+    public void Close()
+    {
+        if (_conn != null)
+        {
+            _conn.Close();
+            _conn = null;
+        }
+    }
+
+    private Dictionary<string, string> Conninfo
+    {
+        get
+        {
+            var connBuilder = new NpgsqlConnectionStringBuilder(_url);
+            return connBuilder
+                .ConnectionString
+                .Split(";")
+                .Select(part => part.Split("="))
+                .ToDictionary(split => split[0].Trim(), split => split[1].Trim());
+        }
+    }
+
+    private T ExecuteScalar<T>(string query)
+    {
+        using (var connection = GetConnection())
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+            return (T)command.ExecuteScalar();
+        }
+    }
+
+    private List<T> ExecuteList<T>(string query, NpgsqlParameter parameter, Func<NpgsqlDataReader, T> selector)
+    {
+        using (var connection = GetConnection())
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+            command.Parameters.Add(parameter);
+            List<T> result = new List<T>();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result.Add(selector(reader));
+                }
+            }
+            return result;
+        }
+    }
+
+    private void Execute(string query)
+    {
+        using (var connection = GetConnection())
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+            command.ExecuteNonQuery();
+        }
+    }
+
+    private string QuoteIdentFull(Ident ident)
+    {
+        if (ident is Table || ident is Sequence)
+        {
+            return $"{QuoteIdent(ident.Schema)}.{QuoteIdent(ident.Name)}";
+        }
+        else
+        {
+            return QuoteIdent(ident);
+        }
+    }
+
+    private string QuoteIdent(string value)
+    {
+        return NpgsqlConnection.GlobalTypeMapper
+            .MapToPgTypeName(value)
+            .ToString();
+    }
+
+    private string DedupLocalHost(string value)
+    {
+        if (Conninfo["host"] == "localhost,localhost" && Conninfo["port"].Split(",").Distinct().Count() == 1)
+        {
+            return value.Split(",")[0];
+        }
+        return value;
+    }
+}
---
+++ Tool/TaskResolver.cs
@@ -0,0 +1,65 @@
+namespace PgSync;
+public class TaskResolver
+{
+    private readonly string[] args;
+    private readonly Options opts;
+    private readonly DataSource source;
+    private readonly DataSource destination;
+    private readonly Dictionary<string, List<string[]>> groups;
+    private readonly string firstSchema;
+    private readonly List<string> notes;
+
+    public TaskResolver(string[] args, Options opts, DataSource source, DataSource destination, Dictionary<string, List<string[]>> groups, string firstSchema)
+    {
+        this.args = args;
+        this.opts = opts;
+        this.source = source;
+        this.destination = destination;
+        this.groups = groups;
+        this.firstSchema = firstSchema;
+        this.notes = new List<string>();
+    }
+
+    public List<Task> ResolveTasks()
+    {
+        List<Task> tasks = new List<Task>();
+
+        // get lists from args
+        var (groups, tables) = ProcessArgs();
+
+        // expand groups into tasks
+        foreach (var group in groups)
+        {
+            tasks.AddRange(GroupToTasks(group));
+        }
+
+        // expand tables into tasks
+        foreach (var table in tables)
+        {
+            tasks.AddRange(TableToTasks(table));
+        }
+
+        // get default if none given
+        if (!opts.Groups && !opts.Tables && args.Length == 0)
+        {
+            tasks.AddRange(DefaultTasks());
+        }
+
+        // resolve any tables that need it
+        foreach (var task in tasks)
+        {
+            task.Table = FullyResolve(task.Table);
+        }
+
+        return tasks;
+    }
+
+    public bool IsGroup(string group)
+    {
+        return groups.ContainsKey(group);
+    }
+
+    // Rest of the methods translation
+    // ...
+}
+}
